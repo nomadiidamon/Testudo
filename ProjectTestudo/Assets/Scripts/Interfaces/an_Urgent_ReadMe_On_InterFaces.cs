db072503819be2a69154e27cacc09beb9a9e9a24
Damon S. Green II <damonsgreen2@gmail.com>
2025-03-08 09:08:19 -0500

    script readmes added

---
+++ ProjectTestudo/Assets/Scripts/Interfaces/an_Urgent_ReadMe_On_InterFaces.cs
@@ -0,0 +1,17 @@
+///                     README!! README!! README!! README!! README!! README!!
+///                                        README!! README!! README!! README!! README!! README!!
+///                        
+/// 
+/// 
+/// 
+///  This directory is built around the interfaces in the    -->   UTILITYSCRIPTS / UTILITYINTERFACES   <--   directory. 
+///  If those LOWER LEVEL interfaces are INDEED YOUR FOCUS...
+///  
+///     Assets --> Scripts --> UtilityScripts --> UtilityInterfaces            <-- is your desired project location.
+///     
+/// 
+/// 
+/// 
+///                         README OVER!! README OVER!! README OVER!! README OVER!! README OVER!! README OVER!!
+///                README OVER!! README OVER!! README OVER!! README OVER!! README OVER!! README OVER!!
+///                        
---
+++ ProjectTestudo/Assets/Scripts/Utility Scripts/UtilityInterfaces/an_Urgent_ReadMe_On_UtilityInterfaces.cs
@@ -0,0 +1,14 @@
+///             README!! README!! README!! README!! README!! README!!
+///                                    README!! README!! README!! README!! README!! README!!
+///                                                          README!! README!! README!! README!! README!! README!!
+/// 
+///      This is a directory for the foundational level of interfaces AS OPPOSED TO 
+///                     the more specefic brand of interfaces found int the      -->   SCRIPTS/INTERFCAES   <--       directory. 
+///                                       If you are truly seeking the specific brand of interfaces...
+///                                                         Assets --> Scripts --> Interfaces         <-- is your desired project location.
+/// 
+///        README OVER!! README OVER!! README OVER!! README OVER!! README OVER!! README OVER!!
+///                      README OVER!! README OVER!! README OVER!! README OVER!! README OVER!! README OVER!!
+///                                           README OVER!! README OVER!! README OVER!! README OVER!! README OVER!! README OVER!!
+///                                                   README OVER!! README OVER!! README OVER!! README OVER!! README OVER!! README OVER!!
+///  
