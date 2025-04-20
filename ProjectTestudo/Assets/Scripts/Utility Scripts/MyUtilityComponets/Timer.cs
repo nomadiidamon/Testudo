f6c1790291f8af107aca6a312f07727ae8f6b654
Damon S. Green II <damonsgreen2@gmail.com>
2025-04-20 11:04:14 -0400

    timers simplified

--- ProjectTestudo/Assets/Scripts/Utility Scripts/MyUtilityComponets/Timer.cs
+++ ProjectTestudo/Assets/Scripts/Utility Scripts/MyUtilityComponets/Timer.cs
@@ -1,47 +1,42 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 [System.Serializable]
 public class Timer : MonoBehaviour
 {
-    public bool isRunning = false;
-    public bool isPaused = false;
-    public bool isFinished = false;
+    public bool isRunning { get; private set; }
+    public bool isPaused { get; private set; }
+    public bool isFinished { get; private set; }
 
-    public float time = 0;
-    public float currentTime = 0;
-    public float timeRemaining = 0;
-    public float elapsedTime = 0;
-    public float elapsedTimeRemaining = 0;
-
+    public float time { get; private set; }
+    public float currentTime { get; private set; }
+    public float elapsedTime { get; private set; }
 
     public void StartTimer(float _time)
     {
         time = _time;
         currentTime = time;
-        timeRemaining = time;
         elapsedTime = 0;
-        elapsedTimeRemaining = 0;
         isRunning = true;
         isPaused = false;
         isFinished = false;
     }
 
-    public void PauseTimer()
-    {
-        isPaused = true;
-    }
-
-    public void ResumeTimer()
-    {
-        isPaused = false;
-    }
-
+    public void PauseTimer() => isPaused = true;
+    public void ResumeTimer() => isPaused = false;
     public void StopTimer()
     {
         isRunning = false;
         isPaused = false;
         isFinished = true;
+    }
+
+    public void ResetTimer()
+    {
+        currentTime = time;
+        elapsedTime = 0;
+        isRunning = false;
+        isPaused = false;
+        isFinished = false;
     }
 
     public void UpdateTimer()
@@ -50,49 +45,28 @@ public class Timer : MonoBehaviour
         {
             currentTime -= Time.deltaTime;
             elapsedTime += Time.deltaTime;
-            timeRemaining = currentTime;
-            elapsedTimeRemaining = elapsedTime;
+
             if (currentTime <= 0)
             {
+                currentTime = 0;
                 StopTimer();
             }
         }
-    }
-
-    public void ResetTimer()
-    {
-        currentTime = time;
-        timeRemaining = time;
-        elapsedTime = 0;
-        elapsedTimeRemaining = 0;
-        isRunning = false;
-        isPaused = false;
-        isFinished = false;
-    }
-
-    public void SetTime(float _time)
-    {
-        time = _time;
-        currentTime = time;
-        timeRemaining = time;
-        elapsedTime = 0;
-        elapsedTimeRemaining = 0;
     }
 
     public void AddTime(float _time)
     {
         time += _time;
         currentTime += _time;
-        timeRemaining = currentTime;
     }
 
     public void SubtractTime(float _time)
     {
-        time -= _time;
-        currentTime -= _time;
-        timeRemaining = currentTime;
+        time = Mathf.Max(0, time - _time);
+        currentTime = Mathf.Max(0, currentTime - _time);
     }
 
-
-
+    public float GetTimeRemaining() => currentTime;
+    public float GetElapsedTime() => elapsedTime;
+    public float GetTotalTime() => time;
 }
